393a904c0f151fb0bb8cccf1e1ae42fdfa824061
Edi Danalache <edi@scalevp.com>
2024-09-19 07:58:20 +0000

    feat: add random movement for bee element
    Co-authored-by: Genie <genie@cosine.sh>

--- assets/js/bee.js
+++ assets/js/bee.js
@@ -5,6 +5,17 @@ function moveBee() {
     var randomX = Math.floor(Math.random() * maxX);
     var randomY = Math.floor(Math.random() * maxY);
     bee.style.transform = "translate(" + randomX + "px, " + randomY + "px)";
+    bee.style.transition = "transform 1s";
+}
+
+setInterval(moveBee, 1000);
+function moveBee() {
+    var bee = document.getElementById("bee");
+    var maxX = window.innerWidth - bee.clientWidth;
+    var maxY = window.innerHeight - bee.clientHeight;
+    var randomX = Math.floor(Math.random() * maxX);
+    var randomY = Math.floor(Math.random() * maxY);
+    bee.style.transform = "translate(" + randomX + "px, " + randomY + "px)";
 }
 
 setInterval(moveBee, 1000);
